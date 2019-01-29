06d3ae4dcd411d7cd74008498b4e393cb0e1e58c
AQIB <aqibrashidpandit@gmail.com>
2019-01-29 12:42:48 +0530

    added tokens page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './Home.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -6,6 +7,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 import SnackNotification from './../../components/SnackNotification';
 import Snackbar from '@material-ui/core/Snackbar';
+import Checkbox from '@material-ui/core/Checkbox';
 
 class Home extends Component {
   scopes = [
@@ -19,6 +21,7 @@ class Home extends Component {
   ];
 
   state = {
+    toTokensView: false,
     identityServerUrl: '',
     redirectUrl: window.location.href,
     clientId: '',
@@ -69,6 +72,8 @@ class Home extends Component {
           refreshToken: response.data.refresh_token
         });
       }
+      this.storeToken(response.data)
+      this.setState({toTokensView: true})
     })
     .catch((error) => {
       this.setState({
@@ -77,7 +82,18 @@ class Home extends Component {
     });
   }
 
-  redirect = () => {
+  storeToken = (data) => {
+    let tokens = localStorage.getItem('tokens');
+    if (tokens) {
+      tokens = JSON.parse(tokens)
+    } else {
+      tokens = []
+    }
+    tokens.push({data: data, type: 'authorization', timestamp: (new Date()).getTime()})
+    localStorage.setItem('tokens', JSON.stringify(tokens))
+  }
+
+  redirectToIdentityServer = () => {
     if (!this.validateForm()) {
       return;
     }
@@ -129,78 +145,97 @@ class Home extends Component {
   }
 
   render() {
+    console.log(this.state)
+    if(this.state.toTokensView) {
+      return <Redirect to="/tokens"></Redirect>
+    }
+
     return (
       <div className="Home">
         { !this.state.code &&
         <section className="Home-form-section">
           <form id="token-generator-form" noValidate autoComplete="off">
-            <TextField
-              id="identity-server-url"
-              label="Identity Server Url"
-              value={this.state.identityServerUrl}
-              onChange={this.handleChange('identityServerUrl')}
-              margin="normal"
-              variant="outlined"
-              type="url"
-            />
-            <TextField
-              id="redirect-url"
-              label="Redirect URL (Read Only)"
-              value={this.state.redirectUrl}
-              onChange={this.handleChange('redirectUrl')}
-              margin="normal"
-              variant="outlined"
-              type="url"
-              inputProps={{
-                readOnly: true,
-                disabled: true,
-              }}
-            />
-            <TextField
-              id="scopes"
-              label="Scopes"
-              value={this.state.scopes}
-              onChange={this.handleChange('scopes')}
-              margin="normal"
-              variant="outlined"
-              type="url"
-            />
-            <TextField
-              id="client-id"
-              label="Client Id"
-              value={this.state.clientId}
-              onChange={this.handleChange('clientId')}
-              margin="normal"
-              variant="outlined"
-              type="text"
-            />
-            <TextField
-              id="client-secret"
-              label="Client Secret"
-              value={this.state.clientSecret}
-              onChange={this.handleChange('clientSecret')}
-              margin="normal"
-              variant="outlined"
-              type="text"
-            />
-            <Snackbar
-              id="errorSnackbar"
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-              }}
-              open={this.state.validationErrMsg.length > 0}
-              autoHideDuration={6000}
-              onClose={() => this.setState({'validationErrMsg': ''})}
-            >
-              <SnackNotification
-                variant="error"
-                message={this.state.validationErrMsg}
+            <div>
+              <TextField
+                id="identity-server-url"
+                label="Identity Server Url"
+                value={this.state.identityServerUrl}
+                onChange={this.handleChange('identityServerUrl')}
+                margin="normal"
+                variant="outlined"
+                type="url"
               />
-            </Snackbar>
-            <Button variant="contained" color="primary" size="large" onClick={this.redirect}>
-              Generate Access Token
-            </Button>
+            </div>
+            <div>
+              <TextField
+                id="redirect-url"
+                label="Redirect URL (Read Only)"
+                value={this.state.redirectUrl}
+                onChange={this.handleChange('redirectUrl')}
+                margin="normal"
+                variant="outlined"
+                type="url"
+                inputProps={{
+                  readOnly: true,
+                  disabled: true,
+                }}
+              />
+            </div>
+            <div>
+              <TextField
+                id="scopes"
+                label="Scopes"
+                value={this.state.scopes}
+                onChange={this.handleChange('scopes')}
+                margin="normal"
+                variant="outlined"
+                type="url"
+              />
+            </div>
+            <div>
+              <TextField
+                id="client-id"
+                label="Client Id"
+                value={this.state.clientId}
+                onChange={this.handleChange('clientId')}
+                margin="normal"
+                variant="outlined"
+                type="text"
+              />
+            </div>
+            <div>
+              <TextField
+                id="client-secret"
+                label="Client Secret"
+                value={this.state.clientSecret}
+                onChange={this.handleChange('clientSecret')}
+                margin="normal"
+                variant="outlined"
+                type="text"
+              />
+            </div>
+            <div>
+              <Snackbar
+                id="errorSnackbar"
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                open={this.state.validationErrMsg.length > 0}
+                autoHideDuration={6000}
+                onClose={() => this.setState({'validationErrMsg': ''})}
+              >
+                <SnackNotification
+                  variant="error"
+                  message={this.state.validationErrMsg}
+                />
+              </Snackbar>
+            </div>
+            <div className="submit-wrapper">
+              <Button variant="contained" color="primary" size="large" onClick={this.redirectToIdentityServer}>
+                Generate Token
+              </Button>
+            </div>
           </form>
         </section> }
         {
--- src/views/TokenList/TokenList.js
+++ src/views/TokenList/TokenList.js
@@ -1,10 +1,165 @@
 import React, { Component } from 'react';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+import './TokenList.css'
+
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: '#282c34',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 
 class TokenList extends Component {
-    render() {
-        return <div>Hello</div>
+    
+  state = {
+    open: false,
+    accessToken: '',
+    refreshToken: ''
+  };
+
+  tokens = () => {
+    let tokens = localStorage.getItem('tokens')
+    if (!tokens) {
+      return []
     }
+    return JSON.parse(tokens).reverse()
+  }
+
+  timestampFormatted = (timestamp) => {
+    return (new Date(timestamp)).toLocaleString()
+  }
+
+  expiresAt = (createdAtTimestamp, expiresIn) => {
+    let expireTimestamp = parseInt(createdAtTimestamp) + parseInt(expiresIn) * 1000
+    let nowTimestamp = (new Date()).getTime()
+    let expiredString = ''
+    if (nowTimestamp >= expireTimestamp) {
+      expiredString = ' (EXPIRED)'
+    }
+    return (new Date(expireTimestamp)).toLocaleString() + expiredString 
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  getToken = (token) => {
+    this.setState({
+      accessToken: token.data.access_token,
+      refreshToken: token.data.refresh_token
+    })
+    this.handleClickOpen()
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <CustomTableCell>#</CustomTableCell>
+                <CustomTableCell align="right">Type</CustomTableCell>
+                <CustomTableCell align="right">Generated At</CustomTableCell>
+                <CustomTableCell align="right">Expires At</CustomTableCell>
+                <CustomTableCell align="right">Token</CustomTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.tokens().map((token, index) => (
+                <TableRow key={index + 1}>
+                  <CustomTableCell component="th" scope="row">
+                    {index + 1}
+                  </CustomTableCell>
+                  <CustomTableCell component="th" scope="row">
+                    {token.type}
+                  </CustomTableCell>
+                  <CustomTableCell align="right">{ this.timestampFormatted(token.timestamp) }</CustomTableCell>
+                  <CustomTableCell align="right">{ this.expiresAt(token.timestamp, token.data.expires_in) }</CustomTableCell>
+                  <CustomTableCell align="right">
+                    <Button size="small" variant="contained" onClick={() => this.getToken(token)}>
+                      VIEW
+                    </Button>
+                  </CustomTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+        <Dialog
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+          fullWidth= { true }
+          maxWidth="md"
+        >
+        <DialogTitle id="alert-dialog-slide-title">
+          Tokens 
+        </DialogTitle>
+        <DialogContent>
+          <div className="token-field">
+            <TextField
+              multiline={ true }
+              label="Access Token"
+              value={this.state.accessToken}
+              margin="normal"
+              variant="outlined"
+              type="text"
+            />
+          </div>
+          { this.state.refreshToken &&
+          <div className="token-field">
+            <TextField
+              multiline={ true }
+              label="Refresh Token"
+              value={this.state.refreshToken}
+              margin="normal"
+              variant="outlined"
+              type="text"
+            />
+          </div>}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </div>
+    )
+  }
 }
 
 export default TokenList;
